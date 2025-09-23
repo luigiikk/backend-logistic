2a44a0124f44e73326d4e0458bf8990a866a9b27
Luigi Mateus <87377118+luigiikk@users.noreply.github.com>
2025-09-22 22:13:26 -0300

    ✨feat: immplements swagger docs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,33 @@
 import { app } from "@/app.js";
+import { fastifyCors } from "@fastify/cors"
+import {validatorCompiler, serializerCompiler} from 'fastify-type-provider-zod'
 import { env } from "@/env/index.js";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
+// input data
+app.setValidatorCompiler(validatorCompiler);
+// output data
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {origin: '*'});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Logistic API',
+      version: '1.0.0',
+    }
+  }
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+});
+
+app.get('/', () => {
+  return 'hello world'
+})
 
 app.listen({
   host: '0.0.0.0',
