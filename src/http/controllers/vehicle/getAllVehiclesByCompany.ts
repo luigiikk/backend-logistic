476fc4c8008a44fe0fac46c44fdfd0439d2cb9db
Leomaan <146137607+Leomaan@users.noreply.github.com>
2026-05-07 14:40:08 -0300

    fix formattedVehicle

--- src/http/controllers/vehicle/getAllVehiclesByCompany.ts
+++ src/http/controllers/vehicle/getAllVehiclesByCompany.ts
@@ -13,14 +13,18 @@ export async function getAllVehiclesByCompany(
       return reply.status(409).send();
     }
 
-    const vehicle = await getAllVehiclesByCompanyService(company_id);
-    const formattedVehicles = vehicle.map((vehicle) => ({
-      id: vehicle.id,
-      plate: vehicle.plate ?? "",
-      model: vehicle.model ?? "",
-      total_volume: vehicle.total_volume,
-      status: vehicle.status.name,
-    }));
+    const vehicles = await getAllVehiclesByCompanyService(company_id);
+    const formattedVehicles = vehicles.map((vehicle) => {
+
+      return {
+        id: vehicle.id,
+        plate: vehicle.plate ?? "",
+        model: vehicle.model ?? "",
+        total_volume: vehicle.total_volume,
+        available_volume: vehicle.available_volume, 
+        status: vehicle.status.name,
+      };
+    });
 
     return reply.status(200).send(formattedVehicles);
   } catch (error) {
